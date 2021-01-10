940b5496ef21e00b9a66c4577f87229506041b3a
Weiming Liu <weiming@mail.ustc.edu.cn>
2021-01-10 08:06:18 +0000

    run cluster nsss

--- run-cluster-abs-nsss.sh
+++ run-cluster-abs-nsss.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-./cluster-abs  --save-to ./nsss_cluster_large.card_abs --threads 90 --dump-centers true --metric mixed_nsss --buckets 169,10000,50000,5000 --history-points 10,10,10,10 --nb-hist-samples-per-round 100,2652,52,2652
+./cluster-abs  --save-to ./nsss_cluster_100000.card_abs --threads 90 --dump-centers true --metric mixed_nsss --buckets 169,100000,100000,100000 --history-points 10,10,10,10 --nb-hist-samples-per-round 100,2652,52,2652
