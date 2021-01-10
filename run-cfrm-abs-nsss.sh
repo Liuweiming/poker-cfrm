5d8148cff6aa216f6b49cec4f680c3f5eeb99d98
Weiming Liu <weiming@mail.ustc.edu.cn>
2021-01-10 06:04:23 +0000

    tested cfrm 1 day with 50000 abs

--- run-cfrm-abs-nsss.sh
+++ run-cfrm-abs-nsss.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 ./cfrm --game-type holem --card-abstraction cluster --card-abstraction-param ./nsss_cluster_large.card_abs \
---runtime 2592000 --checkpoint 3600 --dump-strategy /data/liuwm/cfrm/holdem.limit.2p.game.nsss.strategy \
+--runtime 2592000 --checkpoint 86400 --dump-strategy /data/liuwm/cfrm/holdem.limit.2p.game.nsss_day.strategy \
 --threads 90 --handranks ./handranks.dat --gamedef ./games/holdem.limit.2p.game
