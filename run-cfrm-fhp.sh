106643a670a5e29acac18fb913bcb340def596b1
Weiming Liu <weiming@mail.ustc.edu.cn>
2021-02-11 01:52:16 +0000

    test fhp 2 13.

--- run-cfrm-fhp.sh
+++ run-cfrm-fhp.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
-export OMP_NUM_THREADS=10
+export OMP_NUM_THREADS=180
 
 ./cfrm --game-type holdem \
---runtime 360000 --checkpoint 60  \
+--runtime 360000 --checkpoint 3600  \
+--dump-strategy /data/liuwm/cfrm/fhp_2_13.limit.2p.game.strategy \
 --print-best-response \
 --card-abstraction cluster \
 --card-abstraction-param ./result/is_cluster.card_abs \
---threads 10 --handranks ./handranks.dat \
+--threads 180 --handranks ./handranks.dat \
 --gamedef ./games/fhp.limit.2p.game
--- run-cfrm_leduc_4_13.sh
+++ run-cfrm_leduc_4_13.sh
@@ -3,6 +3,6 @@
 export OMP_NUM_THREADS=20
 
 ./cfrm --game-type leduc \
---runtime 600 --checkpoint 10  --dump-strategy /data/liuwm/cfrm/leduc.limit.2p.game.strategy \
+--runtime 600 --checkpoint 10  \
 --print-best-response \
 --threads 1 --handranks ./handranks.dat --gamedef ./games/leduc_4_13.limit.2p.game
